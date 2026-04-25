497ffcbf3882ba1d9ac40306d9a58e7b5bb76b92
Jamblond <jamblond.contact@gmail.com>
2026-04-25 14:50:07 +0200

    Beta episodeshd.js

--- episodeshd.js
+++ episodeshd.js
@@ -1,8 +1,24 @@
 const episodes = [
     {
+        value: '7.png',
+        text: "Client préféré",
+        description: 'boo! be mine VF episode 7 HD',
+        preview: '7-preview.webp',
+        url: ['7-1.png', '7-2.png', '7-3.png', '7-4.png', '7-5.png'],
+        multi: true
+    },
+    {
+        value: '6.png',
+        text: "Client préféré",
+        description: 'boo! be mine VF episode 6 HD',
+        preview: '6-preview.webp',
+        url: ['6-1.png', '6-2.png'],
+        multi: true
+    },
+    {
         value: '5.png',
         text: "Un GROS Crush",
-        description: 'boo! be mine VF episode 1 HD',
+        description: 'boo! be mine VF episode 5 HD',
         preview: '5-preview.webp',
         url: ['5-1.png', '5-2.png'],
         multi: true
@@ -10,7 +26,7 @@ const episodes = [
     {
         value: '4.png',
         text: "Debout",
-        description: 'boo! be mine VF episode 1 HD',
+        description: 'boo! be mine VF episode 4 HD',
         preview: '4-preview.webp',
         url: ['4-1.png', '4-2.png'],
         multi: true
@@ -18,7 +34,7 @@ const episodes = [
     {
         value: '3.png',
         text: "Présentations",
-        description: 'boo! be mine VF episode 1 HD',
+        description: 'boo! be mine VF episode 3 HD',
         preview: '3-preview.webp',
         url: ['3-1.png', '3-2.png'],
         multi: true
@@ -26,7 +42,7 @@ const episodes = [
     {
         value: '2.png',
         text: "Première rencontre",
-        description: 'boo! be mine VF episode 1 HD',
+        description: 'boo! be mine VF episode 2 HD',
         preview: '2-preview.webp',
         url: ['2-1.png', '2-2.png'],
         multi: true
